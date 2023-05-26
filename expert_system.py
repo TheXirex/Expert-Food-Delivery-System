7d7c070b855fbaff75979d8c1d178a8984b306a6
Vladyslav Volokitin <the.xirex@gmail.com>
2023-05-26 23:46:55 +0300

    Finish project

--- expert_system.py
+++ expert_system.py
@@ -1,7 +1,6 @@
 from tabulate import tabulate as tb
 import os
 
-# done
 class Dish:
     def __init__(self, name, dtype, vn, price, components):
         self.name = name
@@ -12,33 +11,23 @@ class Dish:
     def __str__(self) -> str:
         return f'Title: {self.name}\nComponents: {self.components}\nPrice: ${self.price}'
 
-# done
-def input_database(file_name):
+def input_database(file_name) -> list:
     dishes = []
     with open(file_name) as text:
         for line in text:
             lst = line.split(':')
-
-            name = lst[0][:-1]
-            dtype = lst[1][2:-2]
-            vn= lst[2][2:-2]
-            price= float(lst[3][2:-2])
-            components = [i.strip() for i in lst[4][2:-2].split(',')]
-
-            obj = Dish(name, dtype, vn, price, components)
+            obj = Dish(lst[0][:-1], lst[1][2:-2], lst[2][2:-2], float(lst[3][2:-2]), [i.strip() for i in lst[4][2:-2].split(',')])
             dishes.append(obj)
     return dishes
 
-# done
 def select_types(dishes) -> list:
     types = []
     for dish in dishes:
         if dish.dtype not in types:
             types.append(dish.dtype)
     return types
-
-# done    
-def choose_dish_type(types):
+   
+def choose_dish_type(types) -> str:
 
     table = [[i + 1, types[i]] for i in range(len(types))]
     print(tb(table, ['Type'], tablefmt = 'simple_grid'))
@@ -55,7 +44,6 @@ def choose_dish_type(types):
             else:
                 print('Choose a number from the above.')
 
-# done
 def print_ingridients(lst_ingridients) -> None:
 
     table = []
@@ -70,9 +58,9 @@ def print_ingridients(lst_ingridients) -> None:
         else:
             table.append(row)
             break
+    
     print(tb(table, tablefmt = 'simple_grid'))
 
-# done
 def input_lst_ingridients(dishes):
 
     set_ingridients = []
@@ -83,8 +71,10 @@ def input_lst_ingridients(dishes):
                 set_ingridients.append(ingridient)
 
     while True:
+
         flag = True
         input_ingridients = input('Enter your ingridients separated by "/": ')
+
         if input_ingridients == '':
             print('Empty line, please enter the ingredient(s).')
             flag = False
@@ -112,10 +102,10 @@ def input_lst_ingridients(dishes):
                 if dish not in set_dishes:
                     if ingridient in dish.components:
                         set_dishes.append(dish)
-
+        
+        os.system('cls')
         return set_dishes, lst_ingridients
 
-# done
 def vegeterian():
     while True:
         try:
@@ -128,9 +118,11 @@ def vegeterian():
             else: print('Incorrect value. Enter <1> to confirm or <2> to reject.')
     
 def search(dishes):
+
     if vegeterian(): dishes = [i for i in dishes if i.vn == 'V']
     else: dishes = [i for i in dishes if i.vn == 'N']
-        
+    os.system('cls')
+
     set_ingridients = []
 
     for dish in dishes:
@@ -143,10 +135,13 @@ def search(dishes):
     new_dishes, lst = input_lst_ingridients(dishes)
 
     lst_dishes = algorithm(new_dishes, lst)
-
+    
     dish = choose_dish(lst_dishes)
 
-    return dish
+    if dish is not None:
+        return dish
+    else:
+        return None
 
 
 def algorithm(new_dishes, lst):
@@ -162,21 +157,21 @@ def algorithm(new_dishes, lst):
             for ingridient in dish.components:
                 if ingridient not in lst and ingridient not in set_ingridients:
                     set_ingridients.append(ingridient)
-        
-        print(set_ingridients)
 
         while True:
             try:
                 ans = int(input(f'Do you want to see {set_ingridients[0]} in a dish? (<1> to confirm or <2> to reject): '))
             except:
                 print(set_ingridients[0])
-                print('Incorrect value. Enter <1> to confirm or <2> to reject.3')
+                print('Incorrect value. Enter <1> to confirm or <2> to reject.')
 
             else:
                 if ans == 1 or ans == 2:
                     break
                 else:
-                    print('Incorrect value. Enter <1> to confirm or <2> to reject.4')
+                    print('Incorrect value. Enter <1> to confirm or <2> to reject.')
+
+        os.system('cls')
 
         removed_lst = []
 
@@ -193,37 +188,40 @@ def algorithm(new_dishes, lst):
             dish_lst.remove(i)
         
         removed_ingridients.append(set_ingridients[0])
-
+        
         if len(dish_lst) == 1:
-            print('\nYour dish is ' + dish_lst[0].name + '!')
+            print("I'm going to assume that this dish will work for you:\n")
+            print(dish_lst[0], '\n')
             dish = [dish_lst[0]]
             return [dish, removed_lst]
         
         elif len(dish_lst) < 1 or len(set_ingridients) < 1:
-            print('\nSorry, but there is not enough information about your dish in the database.')
             return [[], removed_lst]
-
 
 def choose_dish(lst_dish):
     if len(lst_dish[0]) == 1:
         ans = int(input('Would you like to add that dish to the check? (<1> to confirm or <2> to reject): '))
+        os.system('cls')
         if ans == 1:
             return lst_dish[0][0]
         else:
             print('I can offer you alternatives with the ingredients you chose:')
             dish = choose_dish_in_removed_lst(lst_dish)
             return dish
+    elif len(lst_dish[0]) == 0 and len(lst_dish[1]) == 0:
+        return None
     else:
+        os.system('cls')
         print('There are no dishes with a similar set of ingredients, but I can offer you alternatives with the ingredients of your choice:')
         dish = choose_dish_in_removed_lst(lst_dish)
         return dish
 
 def choose_dish_in_removed_lst(lst_dish):
     for i in range(len(lst_dish[1])):
-        print(f'{i+1}: {lst_dish[1][i]}')
+        print(f'-------{i+1}-------\n{lst_dish[1][i]}')
         while True:
             try:
-                number = int(input('Enter the number of dish type: ')) - 1
+                number = int(input('\nEnter the number of dish type. (<0> to reject): ')) - 1
             except:
                 print('The answer must be an integer number! Try again.')
             if number == -1:
@@ -234,7 +232,6 @@ def choose_dish_in_removed_lst(lst_dish):
                 else:
                     print('Choose a number from the above.')
             
-# not done
 def add_check(check, dish):
     check.append([dish.name, dish.price])
     print(tb(check, ['Name', 'Price'], tablefmt = 'simple_grid'))
@@ -243,20 +240,23 @@ def add_check(check, dish):
 def print_final_check(check):
     if len(check) != 0:
         print(tb(check, ['Name', 'Price'], tablefmt = 'simple_grid'))
-
+        print(f'Total sum: {sum([dish[1] for dish in check])}')
+        print('Your order is accepted and our manager will call you back soon to clarify the details. Bon appetit!')
     else:
-        print(tb(check, ['Name', 'Price'], tablefmt = 'simple_grid'))
+        print("You haven't chosen anything. If you change your mind, feel free to contact me. Have a great day!")
+    input('\nPress Enter to exit...')
 
-# done
 def next_position():
     while True:
         try:
             ans = int(input('Do you want to continue to choose? (<1> to confirm or <2> to reject): '))
         except:
-            print('Incorrect value. Enter <1> to confirm or <2> to reject.5')
+            print('Incorrect value. Enter <1> to confirm or <2> to reject.')
         else:
+            os.system('cls')
             if ans == 1: return True
-            else: return False
+            elif ans == 2: return False
+            else: print('Incorrect value. Enter <1> to confirm or <2> to reject.')
 
 def main():
     check = []
@@ -268,13 +268,14 @@ def main():
 
         if dish != None:
             check = add_check(check, dish)
-        
-        os.system('cls')
+            os.system('cls')
+        else:
+            os.system('cls')
+            print('There were no matching dishes in the database.')
 
         if not next_position():
             print_final_check(check)
             break
             
-
 if __name__ == '__main__':
     main()
